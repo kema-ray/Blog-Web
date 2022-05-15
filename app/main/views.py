30b4c162d5e5c0bf1fff0cb9ad3df8749ba19df9
rachel <rachelkemuma99@gmail.com>
2022-05-15 16:40:27 +0300

    Add an api endpoint to help you display the random quotes.

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
 from . import main
 from flask import render_template,Flask
+from ..requests import get_quote
 
 
 @main.route('/')
 def index():
     title = 'Blog'
 
-    return render_template('index.html',title=title)
+    random_quote = get_quote()
+
+    return render_template('index.html',title=title,quote=random_quote)
--- app/models.py
+++ app/models.py
@@ -1,10 +1,30 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
+
+class Quote:
+    def __init__(self,quote,author):
+        self.quote=quote
+        self.author=author
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    blogs = db.relationship('Blog',backref='user',lazy='dynamic')
+    secure_password = db.Column(db.String(255),nullable = False)
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.secure_password,password)
 
 
 
@@ -16,6 +36,7 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(255),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     
     def __repr__(self):
         return f'Blog {self.title}'
